0638ce9eb5176e64104fd67217035c69c728b210
hemrekale <emrevekale@gmail.com>
2021-02-12 12:20:54 +0100

    Add Rshiny tut

--- tSNE_tut.py
+++ tSNE_tut.py
@@ -1,5 +1,4 @@
 # [source]
-# https://towardsdatascience.com/an-introduction-to-t-sne-with-python-example-5a3a293108d1
 
 # First Cauchy Distribution Tutorial
 import numpy as np
@@ -24,6 +23,7 @@ vals = cauchy.ppf([0.001, 0.5, 0.999])
 np.allclose([0.001, 0.5, 0.999], cauchy.cdf(vals))
 
 print(__doc__)
+# [source] : https://scikit-learn.org/stable/auto_examples/manifold/plot_compare_methods.html#sphx-glr-auto-examples-manifold-plot-compare-methods-py
 
 from collections import OrderedDict
 from functools import partial
@@ -38,8 +38,9 @@ from sklearn import manifold, datasets
 # Next line to silence pyflakes. This import is needed.
 Axes3D
 
-n_points = 1000
+n_points = 100
 X, color = datasets.make_s_curve(n_points, random_state=0)
+
 n_neighbors = 10
 n_components = 2
 
@@ -81,5 +82,209 @@ for i, (label, method) in enumerate(methods.items()):
     ax.xaxis.set_major_formatter(NullFormatter())
     ax.yaxis.set_major_formatter(NullFormatter())
     ax.axis('tight')
+
+plt.show()
+
+
+# severed sphere 
+from sklearn.utils import check_random_state
+# Next line to silence pyflakes.
+Axes3D
+
+# Variables for manifold learning.
+n_neighbors = 10
+n_samples = 1000
+
+# Create our sphere.
+random_state = check_random_state(0)
+p = random_state.rand(n_samples) * (2 * np.pi - 0.55)
+t = random_state.rand(n_samples) * np.pi
+
+# Sever the poles from the sphere.
+indices = ((t < (np.pi - (np.pi / 8))) & (t > ((np.pi / 8))))
+colors = p[indices]
+x, y, z = np.sin(t[indices]) * np.cos(p[indices]), \
+    np.sin(t[indices]) * np.sin(p[indices]), \
+    np.cos(t[indices])
+
+# Plot our dataset.
+fig = plt.figure(figsize=(15, 8))
+plt.suptitle("Manifold Learning with %i points, %i neighbors"
+             % (1000, n_neighbors), fontsize=14)
+
+ax = fig.add_subplot(251, projection='3d')
+ax.scatter(x, y, z, c=p[indices], cmap=plt.cm.rainbow)
+ax.view_init(40, -10)
+
+sphere_data = np.array([x, y, z]).T
+
+# Perform Locally Linear Embedding Manifold learning
+methods = ['standard', 'ltsa', 'hessian', 'modified']
+labels = ['LLE', 'LTSA', 'Hessian LLE', 'Modified LLE']
+
+for i, method in enumerate(methods):
+    t0 = time()
+    trans_data = manifold\
+        .LocallyLinearEmbedding(n_neighbors, 2,
+                                method=method).fit_transform(sphere_data).T
+    t1 = time()
+    print("%s: %.2g sec" % (methods[i], t1 - t0))
+
+    ax = fig.add_subplot(252 + i)
+    plt.scatter(trans_data[0], trans_data[1], c=colors, cmap=plt.cm.rainbow)
+    plt.title("%s (%.2g sec)" % (labels[i], t1 - t0))
+    ax.xaxis.set_major_formatter(NullFormatter())
+    ax.yaxis.set_major_formatter(NullFormatter())
+    plt.axis('tight')
+
+# Perform Isomap Manifold learning.
+t0 = time()
+trans_data = manifold.Isomap(n_neighbors, n_components=2)\
+    .fit_transform(sphere_data).T
+t1 = time()
+print("%s: %.2g sec" % ('ISO', t1 - t0))
+
+ax = fig.add_subplot(257)
+plt.scatter(trans_data[0], trans_data[1], c=colors, cmap=plt.cm.rainbow)
+plt.title("%s (%.2g sec)" % ('Isomap', t1 - t0))
+ax.xaxis.set_major_formatter(NullFormatter())
+ax.yaxis.set_major_formatter(NullFormatter())
+plt.axis('tight')
+
+# Perform Multi-dimensional scaling.
+t0 = time()
+mds = manifold.MDS(2, max_iter=100, n_init=1)
+trans_data = mds.fit_transform(sphere_data).T
+t1 = time()
+print("MDS: %.2g sec" % (t1 - t0))
+
+ax = fig.add_subplot(258)
+plt.scatter(trans_data[0], trans_data[1], c=colors, cmap=plt.cm.rainbow)
+plt.title("MDS (%.2g sec)" % (t1 - t0))
+ax.xaxis.set_major_formatter(NullFormatter())
+ax.yaxis.set_major_formatter(NullFormatter())
+plt.axis('tight')
+
+# Perform Spectral Embedding.
+t0 = time()
+se = manifold.SpectralEmbedding(n_components=2,
+                                n_neighbors=n_neighbors)
+trans_data = se.fit_transform(sphere_data).T
+t1 = time()
+print("Spectral Embedding: %.2g sec" % (t1 - t0))
+
+ax = fig.add_subplot(259)
+plt.scatter(trans_data[0], trans_data[1], c=colors, cmap=plt.cm.rainbow)
+plt.title("Spectral Embedding (%.2g sec)" % (t1 - t0))
+ax.xaxis.set_major_formatter(NullFormatter())
+ax.yaxis.set_major_formatter(NullFormatter())
+plt.axis('tight')
+
+# Perform t-distributed stochastic neighbor embedding.
+t0 = time()
+tsne = manifold.TSNE(n_components=2, init='pca', random_state=0)
+trans_data = tsne.fit_transform(sphere_data).T
+t1 = time()
+print("t-SNE: %.2g sec" % (t1 - t0))
+
+ax = fig.add_subplot(2, 5, 10)
+plt.scatter(trans_data[0], trans_data[1], c=colors, cmap=plt.cm.rainbow)
+plt.title("t-SNE (%.2g sec)" % (t1 - t0))
+ax.xaxis.set_major_formatter(NullFormatter())
+ax.yaxis.set_major_formatter(NullFormatter())
+plt.axis('tight')
+
+plt.show()
+
+
+#Perplexity: 
+
+# Author: Narine Kokhlikyan <narine@slice.com>
+# License: BSD
+
+print(__doc__)
+
+from matplotlib.ticker import NullFormatter
+
+n_samples = 300
+n_components = 2
+(fig, subplots) = plt.subplots(3, 5, figsize=(15, 8))
+perplexities = [5, 30, 50, 100]
+
+X, y = datasets.make_circles(n_samples=n_samples, factor=.5, noise=.05)
+
+red = y == 0
+green = y == 1
+
+ax = subplots[0][0]
+ax.scatter(X[red, 0], X[red, 1], c="r")
+ax.scatter(X[green, 0], X[green, 1], c="g")
+ax.xaxis.set_major_formatter(NullFormatter())
+ax.yaxis.set_major_formatter(NullFormatter())
+plt.axis('tight')
+
+for i, perplexity in enumerate(perplexities):
+    ax = subplots[0][i + 1]
+    t0 = time()
+    tsne = manifold.TSNE(n_components=n_components, init='random',
+                         random_state=0, perplexity=perplexity)
+    Y = tsne.fit_transform(X)
+    t1 = time()
+    print("circles, perplexity=%d in %.2g sec" % (perplexity, t1 - t0))
+    ax.set_title("Perplexity=%d" % perplexity)
+    ax.scatter(Y[red, 0], Y[red, 1], c="r")
+    ax.scatter(Y[green, 0], Y[green, 1], c="g")
+    ax.xaxis.set_major_formatter(NullFormatter())
+    ax.yaxis.set_major_formatter(NullFormatter())
+    ax.axis('tight')
+
+# Another example using s-curve
+X, color = datasets.make_s_curve(n_samples, random_state=0)
+
+ax = subplots[1][0]
+ax.scatter(X[:, 0], X[:, 2], c=color)
+ax.xaxis.set_major_formatter(NullFormatter())
+ax.yaxis.set_major_formatter(NullFormatter())
+
+for i, perplexity in enumerate(perplexities):
+    ax = subplots[1][i + 1]
+    t0 = time()
+    tsne = manifold.TSNE(n_components=n_components, init='random',
+                         random_state=0, perplexity=perplexity)
+    Y = tsne.fit_transform(X)
+    t1 = time()
+    print("S-curve, perplexity=%d in %.2g sec" % (perplexity, t1 - t0))
+    ax.set_title("Perplexity=%d" % perplexity)
+    ax.scatter(Y[:, 0], Y[:, 1], c=color)
+    ax.xaxis.set_major_formatter(NullFormatter())
+    ax.yaxis.set_major_formatter(NullFormatter())
+    ax.axis('tight')
+
+# Another example using a 2D uniform grid
+x = np.linspace(0, 1, int(np.sqrt(n_samples)))
+xx, yy = np.meshgrid(x, x)
+X = np.hstack([
+    xx.ravel().reshape(-1, 1),
+    yy.ravel().reshape(-1, 1),
+])
+color = xx.ravel()
+ax = subplots[2][0]
+ax.scatter(X[:, 0], X[:, 1], c=color)
+ax.xaxis.set_major_formatter(NullFormatter())
+ax.yaxis.set_major_formatter(NullFormatter())
+
+for i, perplexity in enumerate(perplexities):
+    ax = subplots[2][i + 1]
+    t0 = time()
+    tsne = manifold.TSNE(n_components=n_components, init='random', random_state=0, perplexity=perplexity)
+    Y = tsne.fit_transform(X)
+    t1 = time()
+    print("uniform grid, perplexity=%d in %.2g sec" % (perplexity, t1 - t0))
+    ax.set_title("Perplexity=%d" % perplexity)
+    ax.scatter(Y[:, 0], Y[:, 1], c=color)
+    ax.xaxis.set_major_formatter(NullFormatter())
+    ax.yaxis.set_major_formatter(NullFormatter())
+    ax.axis('tight')
+
 
 plt.show()
