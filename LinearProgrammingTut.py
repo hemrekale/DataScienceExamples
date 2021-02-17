b7456f3514a94fc5bc8084301da9cc3c0a1f7df2
hemrekale <emrevekale@gmail.com>
2021-02-17 20:29:30 +0100

    LP updated

--- LinearProgrammingTut.py
+++ LinearProgrammingTut.py
@@ -1,3 +1,17 @@
+# [source]: https://realpython.com/linear-programming-python/#linear-programming-examples
+
+
+
+# SciPy disadvantages in solving linear programming
+
+# 1 SciPy can’t run various external solvers.
+# 2 SciPy can’t work with integer decision variables.
+# 3 SciPy doesn’t provide classes or functions that facilitate model building. You have to define arrays and matrices, which might be a tedious and error-prone task for large problems.
+# 4 SciPy doesn’t allow you to define maximization problems directly. You must convert them to minimization problems.
+# 5 SciPy doesn’t allow you to define constraints using the greater-than-or-equal-to sign directly. You must use the less-than-or-equal-to instead.
+
+
+# Example1
 from scipy.optimize import linprog
 
 obj = [-1, -2]
@@ -15,3 +29,106 @@ rhs_ineq = [20,  # Red constraint right side
 
 lhs_eq = [[-1, 5]]  # Green constraint left side
 rhs_eq = [15]       # Green constraint right side
+
+bnd = [(0, float("inf")),  # Bounds of x
+       (0, float("inf"))]  # Bounds of y
+
+opt = linprog(c=obj, A_ub=lhs_ineq, b_ub=rhs_ineq,
+              A_eq=lhs_eq, b_eq=rhs_eq, bounds=bnd,
+              method="revised simplex")
+opt
+
+
+# Now an example with PuLP
+from pulp import LpMaximize, LpProblem, LpStatus, lpSum, LpVariable
+# Create the model
+model = LpProblem(name="small-problem", sense=LpMaximize)
+
+# Initialize the decision variables
+x = LpVariable(name="x", lowBound=0)
+y = LpVariable(name="y", lowBound=0)
+
+# Add the constraints to the model
+model += (2 * x + y <= 20, "red_constraint")
+model += (4 * x - 5 * y >= -10, "blue_constraint")
+model += (-x + 2 * y >= -2, "yellow_constraint")
+model += (-x + 5 * y == 15, "green_constraint")
+
+# Add the objective function to the model
+obj_func = x + 2 * y
+model += obj_func
+
+# Add the objective function to the model
+model += x + 2 * y
+
+#or for larger models 
+# Add the objective function to the model
+model += lpSum([x, 2 * y])
+
+# Solve the problem
+status = model.solve()
+
+# print results
+
+print(f"status: {model.status}, {LpStatus[model.status]}")
+print(f"objective: {model.objective.value()}")
+
+
+for var in model.variables():
+    print(f"{var.name}: {var.value()}")
+
+for name, constraint in model.constraints.items():
+    print(f"{name}: {constraint.value()}")
+
+model.variables()
+model.variables()[0]
+model.variables()[1]
+
+# Example 2 Profit Example
+# SciPy
+obj = [-20, -12, -40, -25]
+
+lhs_ineq = [[1, 1, 1, 1],  # Manpower
+            [3, 2, 1, 0],  # Material A
+            [0, 1, 2, 3]]  # Material B
+
+rhs_ineq = [ 50,  # Manpower
+            100,  # Material A
+             90]  # Material B
+
+opt = linprog(c=obj, A_ub=lhs_ineq, b_ub=rhs_ineq,
+              method="revised simplex")
+opt
+
+
+# PuLP solution to example 2
+
+# Define the model
+model = LpProblem(name="resource-allocation", sense=LpMaximize)
+
+# Define the decision variables
+x = {i: LpVariable(name=f"x{i}", lowBound=0) for i in range(1, 5)}
+
+# Add constraints
+model += (lpSum(x.values()) <= 50, "manpower")
+model += (3 * x[1] + 2 * x[2] + x[3] <= 100, "material_a")
+model += (x[2] + 2 * x[3] + 3 * x[4] <= 90, "material_b")
+
+# Set the objective
+model += 20 * x[1] + 12 * x[2] + 40 * x[3] + 25 * x[4]
+
+# Solve the optimization problem
+status = model.solve()
+
+# Get the results
+print(f"status: {model.status}, {LpStatus[model.status]}")
+print(f"objective: {model.objective.value()}")
+
+for var in x.values():
+    print(f"{var.name}: {var.value()}")
+
+for name, constraint in model.constraints.items():
+    print(f"{name}: {constraint.value()}")
+
+
+
